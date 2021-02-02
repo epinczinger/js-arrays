afaef05478642cb8ddd448de1f5d2a4a781854ae
Eugenia <mariaedallasta@gmail.com>
2021-02-02 19:09:33 -0300

    Add Exercises

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
 'use strict';
 
-const movies = ['ET', 'Batman', 'Love'];
+// const movies = ['ET', 'Batman', 'Love'];
 
-movies[3] = 'Spiderman';
+// movies[3] = 'Spiderman';
 
-movies.push('another movie');
+// movies.push('another movie');
 
-console.log(movies);
+// movies[3] = 'Lion';
+
+// function workWithMovies() {   
+//     return movies;
+// };
+
+// const resultValue = workWithMovies();
+
+// console.log(resultValue.length);
+
+
+let acc = 0;
+for (let i = 1; i < 11; i++) {
+
+  acc = acc + 2;
+  console.log(acc);
+}
+
+
+
+let lunaDelCazador = 2017;
+
+for (let i = 0; i < 15; i++){
+
+    lunaDelCazador += 3; 
+    console.log(lunaDelCazador);
+}
