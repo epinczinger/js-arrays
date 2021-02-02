d06cd3901a5930243f1c73559ae8e598b17d351a
Eugenia <mariaedallasta@gmail.com>
2021-02-02 20:04:15 -0300

    Add exercises

--- main.js
+++ main.js
@@ -17,19 +17,54 @@
 // console.log(resultValue.length);
 
 
-let acc = 0;
-for (let i = 1; i < 11; i++) {
+// let acc = 0;
+// for (let i = 1; i < 11; i++) {
 
-  acc = acc + 2;
-  console.log(acc);
-}
+//   acc = acc + 2;
+//   console.log(acc);
+// }
 
 
 
-let lunaDelCazador = 2017;
+// let lunaDelCazador = 2017;
 
-for (let i = 0; i < 15; i++){
+// for (let i = 0; i < 15; i++){
 
-    lunaDelCazador += 3; 
-    console.log(lunaDelCazador);
-}
+//     lunaDelCazador += 3; 
+//     console.log(lunaDelCazador);
+// }
+
+// let array = [5, 7, 56, 23, 1, 5];
+
+// let total = 0;
+
+// for (let i = 0; i < array.length; i++) {
+
+//    total = total + array[i];
+// }
+// let media = total/array.length;
+//     console.log(media);
+
+// Ejercicio 6
+
+const inputElement = document.querySelector('.js-input');
+const buttonElement = document.querySelector('.js-button');
+const arrayElement = [];
+
+function handleButton() {
+
+    
+    const valueElement = inputElement.value;
+
+    arrayElement.push(valueElement);
+
+    console.log(valueElement);
+    
+    for (const movie of arrayElement) {
+        console.log(`${movie} 'favorita'`)
+    }
+ };
+
+console.log(arrayElement);
+
+buttonElement.addEventListener('click', handleButton);
