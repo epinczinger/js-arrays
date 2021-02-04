b658edda839c4f799d2d94ece101ed77e2a1462c
Eugenia <mariaedallasta@gmail.com>
2021-02-04 10:08:19 -0300

    Add excercises

--- main.js
+++ main.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const buttonElement = document.querySelectorAll('button');
+const bodyElement = document.querySelector('.js-body');
+
+function handleButton() {
+
+    bodyElement.classList.toggle('body-aqua');
+   
+};
+
+   for (const button of buttonElement) {
+       button.addEventListener('click', handleButton);
+};
+
+// console.log(buttonElement);
+
+
+//-----------------
+
+
+
+const items = [
+  'Ada',
+  1815,
+  ['Informática', 'Matemática', 'Escritora'],
+  {
+    mother: 'Anna Isabella',
+    father: 'Lord Byron'
+  }
+];
+
+for (let index = 0; index < items.length; index++) {
+    
+    console.log(`El dato ${items[index]} está en la posición ${index} y es de tipo ${typeof (items[index])}`);
+    
+    
+}
